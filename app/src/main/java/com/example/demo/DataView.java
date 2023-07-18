45d69b2e6faf8c8a1932a7f4cfe21381f38577b4
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2023-07-18 14:25:30 +0100

    Infinite Scroll Screen!

--- app/src/main/java/com/example/demo/DataView.java
+++ app/src/main/java/com/example/demo/DataView.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.provider.BaseColumns;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +25,14 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class DataView extends Fragment {
+
+    boolean isLoading = false;
+    RecyclerView recyclerView;
+    ListViewAdapter adapter;
+    List<String> numbas;
+    DBHelper Helper;
+    SQLiteDatabase DB;
+
 
     public DataView() {
         // Required empty public constructor
@@ -52,16 +61,22 @@ public class DataView extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        DBHelper Helper = new DBHelper(getContext());
-        SQLiteDatabase DB = Helper.getReadableDatabase();
+        Helper = new DBHelper(getContext());
+        DB = Helper.getReadableDatabase();
 
+        numbas = initData(Helper, DB, "0", "10");
+        initialiseRecycler(numbas);
+        initScrollListener();
+    }
+
+    public List<String> initData(DBHelper Helper, SQLiteDatabase DB, String minID, String maxID) {
         String[] projection = {
                 BaseColumns._ID,
                 DBContract.ExampleColumns.COLUMN_NAME_TITLE
         };
 
-        String selection = BaseColumns._ID + " < ?";
-        String[] selectionArgs = { "20" };
+        String selection = BaseColumns._ID + " < ? AND " + BaseColumns._ID + " > ?";
+        String[] selectionArgs = {maxID, minID};
 
         Cursor cursor = DB.query(
                 DBContract.ExampleColumns.TABLE_NAME,   // The table to query
@@ -73,20 +88,104 @@ public class DataView extends Fragment {
                 null                    // don't sort
         );
 
-        List<String> numbas = new ArrayList<>();
+        numbas = new ArrayList<String>();
         while (cursor.moveToNext()) {
             String numba = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.ExampleColumns.COLUMN_NAME_TITLE));
             numbas.add(numba);
         }
         cursor.close();
-        String[] data = numbas.toArray(new String[0]);
+        //String[] data = numbas.toArray(new String[0]);
 
+        return numbas;
+    }
+
+    public void initialiseRecycler(List<String> numbas) {
         // set up the RecyclerView
-        RecyclerView recyclerView = getView().findViewById(R.id.rv_list);
+        recyclerView = getView().findViewById(R.id.rv_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        ListViewAdapter adapter = new ListViewAdapter(data);
-        //adapter.setClickListener(this);
+        adapter = new ListViewAdapter(numbas);
         recyclerView.setAdapter(adapter);
+    }
+
+    public void getData(String minID, String maxID) {
+
+        String[] projection = {
+                BaseColumns._ID,
+                DBContract.ExampleColumns.COLUMN_NAME_TITLE
+        };
+
+        String selection = BaseColumns._ID + " < ? AND " + BaseColumns._ID + " > ?";
+        String[] selectionArgs = {maxID, minID};
+
+        Cursor cursor = DB.query(
+                DBContract.ExampleColumns.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                null                    // don't sort
+        );
+
+        while (cursor.moveToNext()) {
+            String numba = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.ExampleColumns.COLUMN_NAME_TITLE));
+            numbas.add(numba);
+        }
+        cursor.close();
+    }
+
+    private void initScrollListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                System.out.println("Scrolling");
+
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == adapter.getItemCount() - 1) {
+                        //bottom of list!
+                        loadMore();
+                        isLoading = true;
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        numbas.add(null);
+        adapter.notifyItemInserted(numbas.size() - 1);
+        System.out.println("Progress bar should show up.");
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                numbas.remove(numbas.size() - 1);
+                int scrollPosition = numbas.size();
+                adapter.notifyItemRemoved(scrollPosition);
+                int currentSize = scrollPosition;
+                int nextLimit = currentSize + 10;
+
+                getData(Integer.toString(currentSize), Integer.toString(nextLimit));
+
+                //while (currentSize - 1 < nextLimit) {
+                //    rowsArrayList.add("Item " + currentSize);
+                //    currentSize++;
+                //}
+
+                adapter.notifyItemRangeInserted(scrollPosition, numbas.size());
+                //adapter.notifyDataSetChanged();
+                isLoading = false;
+            }
+        }, 2000);
 
     }
 }
--- app/src/main/java/com/example/demo/ListViewAdapter.java
+++ app/src/main/java/com/example/demo/ListViewAdapter.java
@@ -3,24 +3,30 @@ package com.example.demo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.demo.R;
 
-public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHolder> {
+import java.util.List;
 
-    private String[] localDataSet;
+public class ListViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public List<String> localDataSet;
+    private final int VIEW_TYPE_ITEM = 0;
+    private final int VIEW_TYPE_LOADING = 1;
 
     /**
      * Provide a reference to the type of views that you are using
      * (custom ViewHolder)
      */
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ListViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
 
-        public ViewHolder(View view) {
+        public ListViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
 
@@ -32,38 +38,74 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHo
         }
     }
 
+    private class LoadingViewHolder extends RecyclerView.ViewHolder {
+
+        ProgressBar progressBar;
+
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+    }
+
     /**
      * Initialize the dataset of the Adapter
      *
      * @param dataSet String[] containing the data to populate views to be used
      * by RecyclerView
      */
-    public ListViewAdapter(String[] dataSet) {
+    public ListViewAdapter(List<String> dataSet) {
         localDataSet = dataSet;
     }
 
     // Create new views (invoked by the layout manager)
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view, which defines the UI of the list item
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.list_item, viewGroup, false);
 
-        return new ViewHolder(view);
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        if (viewType == VIEW_TYPE_ITEM) {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.list_item, viewGroup, false);
+            return new ListViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.item_loading, viewGroup, false);
+            return new LoadingViewHolder(view);
+        }
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, final int position) {
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText(localDataSet[position]);
+        if (viewHolder instanceof ListViewHolder) {
+            setListItemText((ListViewHolder) viewHolder, position);
+        } else if (viewHolder instanceof LoadingViewHolder) {
+            showLoadingView((LoadingViewHolder) viewHolder, position);
+        }
+    }
+
+    public void setListItemText(ListViewHolder viewHolder, final int position) {
+        viewHolder.getTextView().setText(localDataSet.get(position));
+    }
+
+    public void showLoadingView(LoadingViewHolder viewHolder, int position) {
+        //ProgressBar would be displayed
+        ;
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return localDataSet.length;
+        return localDataSet == null ? 0 : localDataSet.size();
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        return localDataSet.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+    }
+
 }
